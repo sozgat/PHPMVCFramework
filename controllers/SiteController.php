380338d33b8bedeb8adf7b2c1c975deb92e0275e
said.ozgat <said.ozgat@hotmail.com>
2020-11-24 18:36:34 +0300

    +Created Controller and some changes

---
+++ controllers/SiteController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\core\Application;
+use app\core\Controller;
+use app\core\Request;
+
+class SiteController extends Controller
+{
+    public function home()
+    {
+        $params = [
+            'name' => 'Said'
+        ];
+        return $this->render('home', $params);
+    }
+
+    public function contact()
+    {
+        return $this->render('contact');
+    }
+
+    public function handleContact(Request $request)
+    {
+        $body = $request->getBody();
+        var_dump($body);
+        exit();
+        return 'Handling submitted data';
+    }
+
+
+}
